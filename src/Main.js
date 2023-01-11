15b140403453e3c19313b85d18310d84bc1b4ad0
Michael Pace <michael.g.pace@gmail.com>
2023-01-11 00:39:25 +0000

    proper hb comp added to main

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import data from './data.json';
 
 class Main extends React.Component {
     render() {
+
+        let beastArray = [];
+        data.forEach((newBeast, index) => {
+            beastArray.push(
+                <HornedBeast title={newBeast.title}  imageUrl={newBeast.imageUrl} description={newBeast.description} key={index} />
+            )
+        })
+
+
         return(
             <>
 
-            <HornedBeast  title="TitleOne"  imageUrl="w.fakelink.com" description="fakeAF"/>
+            <HornedBeast  title={title}  imageUrl="w.fakelink.com" description="fakeAF" key=index />
             <HornedBeast  title="TitleTwo"  imageUrl="w.fakelink2.com" description="fakeAFToo"/>
 
             </>
