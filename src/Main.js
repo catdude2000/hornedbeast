da51687ad71254b45022b217636c738cc856b34b
Michael Pace <michael.g.pace@gmail.com>
2023-01-11 00:41:20 +0000

    fixed imageurl in hb comp, info and images render

--- src/Main.js
+++ src/Main.js
@@ -8,16 +8,17 @@ class Main extends React.Component {
         let beastArray = [];
         data.forEach((newBeast, index) => {
             beastArray.push(
-                <HornedBeast title={newBeast.title}  imageUrl={newBeast.imageUrl} description={newBeast.description} key={index} />
+                <HornedBeast title={newBeast.title}  imageUrl={newBeast.image_url} description={newBeast.description} key={index} />
             )
         })
 
 
         return(
             <>
+            {beastArray}
 
-            <HornedBeast  title={title}  imageUrl="w.fakelink.com" description="fakeAF" key=index />
-            <HornedBeast  title="TitleTwo"  imageUrl="w.fakelink2.com" description="fakeAFToo"/>
+            {/* <HornedBeast  title={title}  imageUrl="w.fakelink.com" description="fakeAF" key=index />
+            <HornedBeast  title="TitleTwo"  imageUrl="w.fakelink2.com" description="fakeAFToo"/> */}
 
             </>
         )
