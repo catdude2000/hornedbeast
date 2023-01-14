c511e55a3c87cde0e56ca862117d373132bf11ad
Michael Pace <michael.g.pace@gmail.com>
2023-01-14 16:50:14 +0000

    added constructor for props in app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
+}
+
+
 
 function App() {
   return (
