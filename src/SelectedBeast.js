156c49aab2beb5213e385b5d75cfd97eabc54b22
Michael Pace <michael.g.pace@gmail.com>
2023-01-16 00:20:20 +0000

    put props constructor in sb comp

--- src/SelectedBeast.js
+++ src/SelectedBeast.js
@@ -3,14 +3,21 @@ import { Modal } from 'react-bootstrap';
 
 class SelectedBeast extends React.Component {
 
-
+  constructor(props){
+    super(props);
+    
+  }
 
     render () {
         return (
           <Modal show={this.props.handleOnShow} onHide={this.props.handleClose}>
-            <Modal.Header  onClick={this.props.handleClose}>
+            <Modal.Header  closeButton>
               <Modal.Title>dsfsdf{this.props.title}</Modal.Title>
             </Modal.Header>
+            <Modal.Body>
+              <Image src={this.props.beast.imageUrl} />
+              {this.props.beast.description}
+            </Modal.Body>
           </Modal>
         )
     }
