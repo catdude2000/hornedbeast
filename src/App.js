5edf6473051e9ea6c43c2ecfd0b73f7fbde26fc4
tektechnologies <info@tektechnologies.com>
2023-01-16 18:31:04 -0600

    add arrow functions

--- src/App.js
+++ src/App.js
@@ -1,50 +1,47 @@
-import './App.css';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import React from 'react';
-import data from './data.json'
-import SelectedBeast from './SelectedBeast';
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import React from "react";
+import data from "./data.json";
+import SelectedBeast from "./SelectedBeast";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       showModal: false,
-      modalBeast: ''
-    }
+      modalBeast: "",
+    };
   }
 
+  handleOnShow= () => {
+    this.setState({
+      showModal: true,
+      // modalBeast: modalBeast
+    });
+  }
 
-handleOnShow () {
-  this.setState({
-    showModal: true,
-    // modalBeast: modalBeast
-  });
-}
+  handleClose = () => {
+    this.setState({
+      showModal: false,
+    });
+  }
 
-handleClose () {
-  this.setState({
-    showModal: false
-  });
-}
-
-render () {
-  return (
-    <div className="App">
-      <Header/>
-      <Main
-      modalBeast={this.modalBeast}
-      // handleOnShow={this.handleOnShow}
-      data={data}
-      />
-      <Footer/>
-      <SelectedBeast
-      handleOnShow={this.handleOnShow}/>
- 
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main
+          modalBeast={this.modalBeast}
+          // handleOnShow={this.handleOnShow}
+          data={data}
+        />
+        <Footer />
+        <SelectedBeast handleOnShow={this.handleOnShow} />
+      </div>
+    );
+  }
 }
 
 export default App;
