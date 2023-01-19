0ad20ef194fbf9b2dcdc9ed0aeb26cc285a8cde3
Michael Pace <michael.g.pace@gmail.com>
2023-01-19 02:33:34 +0000

    commit for lab3 submission, image still not rendering in modal

--- src/SelectedBeast.js
+++ src/SelectedBeast.js
@@ -10,15 +10,13 @@ class SelectedBeast extends React.Component {
             <Modal.Header closeButton onClick={this.props.handleClose}>
               <Modal.Title>{this.props.modalBeast.title}</Modal.Title>
             </Modal.Header>
+
             <Modal.Body>
+                <Image src={this.props.modalBeast.imageUrl} alt={this.props.modalBeast.description}/>
+             
               
-              <Image src={this.props.modalBeast.imageUrl} />
-              {this.props.modalBeast.description}
             </Modal.Body>
-            {/* <Modal.Footer
-              onClick={this.props.handleClose}>
-              Close Modal
-            </Modal.Footer> */}
+            <Modal.Footer>{this.props.modalBeast.description}</Modal.Footer>
           </Modal>
         )
     }
