9b2421fd9127ff4874ca26a263ef704e0db59cab
Michael Pace <michael.g.pace@gmail.com>
2023-01-20 00:06:22 +0000

    select dropdown appears, handleselect func made

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import HornedBeast from './HornedBeast';
-import { Container, ListGroup, Row, Form } from 'react-bootstrap';
+import React from "react";
+import HornedBeast from "./HornedBeast";
+import { Container, ListGroup, Row, Form } from "react-bootstrap";
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sortedHorns: this.props.data,
+            hornNum: ""
+        }
+    }
 
+        handleSelect = (event) => {
+            let selected = event.target.value;
+            if (selected === 1) {
+                ///////////////////////////////// .horns??
+                let newData = this.props.data.horns.filter((number) => number);
+                this.setState({ sortedHorns: newData });
+            }   else if (selected === 2) {
+                ///////////////////////////////// .horns??
+                let newData = this.props.data.horns.filter((number) => number);
+                this.setState({ sortedHorns: newData });
+            }   else if (selected === 3) {
+                ///////////////////////////////// .horns??
+                let newData = this.props.data.horns.filter((number) => number);
+                this.setState({ sortedHorns: newData });
+            } else if (selected === 100) {
+                ///////////////////////////////// .horns??
+                let newData = this.props.data.horns.filter((number) => number);
+                this.setState({ sortedHorns: newData });
+            } else {
+                this.setState({ sortedHorns: this.props.data });
+            }
+        };
     render() {
-        let dispHorns = this.props.sortedHorns.map((number, index) => { 
-            return <ListGroup.Item key={index}>{number}</ListGroup.Item>})
+
+        // let dispHorns = this.props.sortedHorns.map((number, index) => { 
+        //     return <ListGroup.Item key={index}>{number}</ListGroup.Item>})
+
         return(
             <>
-            <ListGroup>{dispHorns}</ListGroup>
+            {/* <ListGroup>{dispHorns}</ListGroup> */}
             <Form onSubmit={this.handleSubmit}>
-
+                <Form.Label>
+                    {/* //////////////////?????????? need label? */}
+                </Form.Label>
+                <Form.Select name='selected' onChange={this.handleSelect}>
+                    <option value={0}>0</option>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={100}>100</option>
+                </Form.Select>
             </Form>
             <Container>
                 <Row>
-                {this.props.data.map((beast, index) => (
+                {this.state.sortedHorns.map((beast, index) => (
                     <HornedBeast 
                     title={beast.title}  
                     imageUrl={beast.image_url} 
